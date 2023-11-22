ce590cb64c20eed94f3699d8bbc42690d05d7e22
Tegar Machfudzi <v-tegarmachfudzi@anteraja.id>
2023-11-22 10:46:41 +0700

    create new employe system api

---
+++ src/main/java/com/studydaily/employesystemapi/EmployeSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.studydaily.employesystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studydaily/employesystemapi/EmployeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studydaily.employesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
